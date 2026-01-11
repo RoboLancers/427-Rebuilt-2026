35c216e3548f3b3c058612d45c36d8a47a678319
Connor Glazer <deerliver.contact@gmail.com>
2026-01-11 16:01:03 -0500

    Fixed whatever tomfoolery was happening earlier
    Co-authored-by: Miles G <retropro29@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -2,28 +2,41 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+
+
+
+
 package frc.robot.subsystems.swervedrive;
 
 import java.io.File;
 import java.util.function.DoubleSupplier;
 
-import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import swervelib.parser.SwerveParser;
 import swervelib.SwerveDrive;
+import swervelib.math.SwerveMath;
+import swervelib.parser.SwerveParser;
+
 
 
 
 public class SwerveSubsystem extends SubsystemBase {
   double maximumSpeed = Units.feetToMeters(4.5);
-  File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(),"swerve");
-  SwerveDrive  swerveDrive = new SwerveParser(directory).createSwerveDrive(maximumSpeed);
+  private final SwerveDrive  swerveDrive;
 
   /** Creates a new SwerveSubsystem. */
-  public SwerveSubsystem() {
+  public SwerveSubsystem(File directory) {
+    File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(),"swerve");
+    try {
+    swerveDrive = new SwerveParser(directory).createSwerveDrive(maximumSpeed);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+
+
   }
 
   @Override
@@ -36,11 +49,11 @@ public class SwerveSubsystem extends SubsystemBase {
       Translation2d scaledInputs = SwerveMath.scaleTranslation(new Translation2d(translationX.getAsDouble(), translationY.getAsDouble()), 0.8);
  
         //Make the robot move
-        driveFieldOriented (swerveDrive.swerveController.getTargetSpeeds(scaledInputs.getX(), scaledInputs.getY(),
+        swerveDrive.driveFieldOriented(swerveDrive.swerveController.getTargetSpeeds(scaledInputs.getX(), scaledInputs.getY(),
                             headingX.getAsDouble(),
                             headingY.getAsDouble(),
                             swerveDrive.getOdometryHeading().getRadians(),
-                            swerveDrive.getMaximumVelocity()));
+                            swerveDrive.getMaximumChassisVelocity()));
   });
 }
   /**
@@ -54,9 +67,9 @@ public class SwerveSubsystem extends SubsystemBase {
   public Command driveCommand(DoubleSupplier translationX, DoubleSupplier translationY, DoubleSupplier angularRotationX) {
   return run(() -> {
     //Make the robot move
-    swerveDrive.drive(new Translation2d(translationX.getAsDouble() * swerveDrive.getMaximumVelocity(),
-                                        translationX.getAsDouble() * swerveDrive.getMaximumVelocity()),
-                                        angularRotationX.getAsDouble() * swerveDrive.getMaximumAngularVelocity(),
+    swerveDrive.drive(new Translation2d(translationX.getAsDouble() * swerveDrive.getMaximumChassisVelocity(),
+                                        translationX.getAsDouble() * swerveDrive.getMaximumChassisVelocity()),
+                                        angularRotationX.getAsDouble() * swerveDrive.getMaximumChassisAngularVelocity(),
                                         true,
                                         false);
   });
