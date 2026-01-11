7f2a65a1ea78e845fec2df92403bb076c6bb6c7c
Connor Glazer <deerliver.contact@gmail.com>
2026-01-11 14:33:49 -0500

    Mostly Compleated swerve code:  -Filled out .json configs  -Compleated? swerveSubsystem.java needs to add real data + sim
    Co-authored-by: Miles G <retropro29@users.noreply.github.com>

---
+++ src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -0,0 +1,64 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.swervedrive;
+
+import java.io.File;
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import swervelib.parser.SwerveParser;
+import swervelib.SwerveDrive;
+
+
+
+public class SwerveSubsystem extends SubsystemBase {
+  double maximumSpeed = Units.feetToMeters(4.5);
+  File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(),"swerve");
+  SwerveDrive  swerveDrive = new SwerveParser(directory).createSwerveDrive(maximumSpeed);
+
+  /** Creates a new SwerveSubsystem. */
+  public SwerveSubsystem() {
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+  public Command driveCommand(DoubleSupplier translationX, DoubleSupplier translationY, DoubleSupplier headingX, DoubleSupplier headingY) {
+    return run(() -> {
+       
+      Translation2d scaledInputs = SwerveMath.scaleTranslation(new Translation2d(translationX.getAsDouble(), translationY.getAsDouble()), 0.8);
+ 
+        //Make the robot move
+        driveFieldOriented (swerveDrive.swerveController.getTargetSpeeds(scaledInputs.getX(), scaledInputs.getY(),
+                            headingX.getAsDouble(),
+                            headingY.getAsDouble(),
+                            swerveDrive.getOdometryHeading().getRadians(),
+                            swerveDrive.getMaximumVelocity()));
+  });
+}
+  /**
+   * Command to drive the robot using translative values and heading as angular velocity
+   * 
+   * @param translationX       Translation in the X direction
+   * @param translationY       Translation in the Y direction
+   * @param angularRotationX   Rotation of the robot to set
+   * @return Drive command.
+   */
+  public Command driveCommand(DoubleSupplier translationX, DoubleSupplier translationY, DoubleSupplier angularRotationX) {
+  return run(() -> {
+    //Make the robot move
+    swerveDrive.drive(new Translation2d(translationX.getAsDouble() * swerveDrive.getMaximumVelocity(),
+                                        translationX.getAsDouble() * swerveDrive.getMaximumVelocity()),
+                                        angularRotationX.getAsDouble() * swerveDrive.getMaximumAngularVelocity(),
+                                        true,
+                                        false);
+  });
+  }
+}
