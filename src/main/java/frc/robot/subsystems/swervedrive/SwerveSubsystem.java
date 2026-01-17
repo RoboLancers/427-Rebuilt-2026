c610549aa77de4a79c8d1344839f496f5c4eddaa
Connor Glazer <deerliver.contact@gmail.com>
2026-01-17 11:08:02 -0500

    Implement AutoBalance and advanced drive commands for SwerveSubsystem; update Constants and joystick configurations
    #2
    Co-authored-by: Miles G <retropro29@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -13,11 +13,13 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Config;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 import swervelib.SwerveDrive;
 import swervelib.SwerveDriveTest;
 import swervelib.SwerveInputStream;
@@ -91,6 +93,8 @@ public class SwerveSubsystem extends SubsystemBase {
    * @param translationY       Translation in the Y direction
    * @param angularRotationX   Rotation of the robot to set
    * @return Drive command.
+   * 
+   * IT DOES SOMETHING
    */
 
   public Command driveCommand(DoubleSupplier translationX, DoubleSupplier translationY, DoubleSupplier angularRotationX) {
@@ -104,6 +108,8 @@ public class SwerveSubsystem extends SubsystemBase {
                                         false);
   });
   }
+
+
 
     public void driveFieldOriented(ChassisSpeeds velocity)
   {
@@ -122,9 +128,35 @@ public class SwerveSubsystem extends SubsystemBase {
     });
   }
 
+  public void drive(ChassisSpeeds velocity) {
+    swerveDrive.driveFieldOriented(velocity);
+  }
+
+  public Pose2d getPose(){
+    return swerveDrive.getPose();
+  }
+
+  public Rotation2d getHeading(){
+    return getPose().getRotation();
+  }
+
+  public ChassisSpeeds getTargetSpeeds(double xInput, double yInput, double headingX, double headingY){
+    Translation2d scaledInputs = SwerveMath.cubeTranslation(new Translation2d(xInput, yInput));
+    return swerveDrive.swerveController.getTargetSpeeds(scaledInputs.getX(),
+                                                        scaledInputs.getY(),
+                                                        headingX, headingY,
+                                                        getHeading().getRadians(),
+                                                        Constants.MAX_SPEED);
+  }
+
   public void zeroGyro() {
     swerveDrive.zeroGyro();
   }
+
+  public SwerveDriveKinematics getKinematics() {
+    return swerveDrive.kinematics;
+  }
+
 
   public void drive(Translation2d translation, double Rotation, boolean fieldReletive) {
     swerveDrive.drive(translation,
@@ -166,6 +198,10 @@ public Object resetOdometry(Pose2d pose2d) {
 public Command centerModulesCommand() {
     // TODO Auto-generated method stub
     throw new UnsupportedOperationException("Unimplemented method 'centerModulesCommand'");
+}
+public ChassisSpeeds getTargetSpeeds(double asDouble, double asDouble2, Rotation2d rotation2d) {
+  // TODO Auto-generated method stub
+  throw new UnsupportedOperationException("Unimplemented method 'getTargetSpeeds'");
 }
 
 
