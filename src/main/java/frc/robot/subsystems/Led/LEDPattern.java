7a58382cef47bf74a889733879568f70f3bd4eaa
Connor Glazer <deerliver.contact@gmail.com>
2026-02-07 11:17:10 -0500

    Setup LEDSubsystem to display a solid red LED strip of 60 LEDS
    Co-authored-by: Miles G <retropro29@users.noreply.github.com>

---
+++ src/main/java/frc/robot/subsystems/Led/LEDPattern.java
@@ -0,0 +1,14 @@
+/*
+
+package frc.robot.subsystems.Led;
+
+import edu.wpi.first.wpilibj.util.Color;
+
+public class LEDPattern{
+    
+LEDPattern red = LEDPattern.solid(Color.kRed);
+
+red.applyTo(m_ledBuffer);
+    
+}
+*/
--- src/main/java/frc/robot/subsystems/Led/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/Led/LEDSubsystem.java
@@ -10,30 +10,50 @@ import edu.wpi.first.wpilibj.LEDPattern;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import swervelib.parser.SwerveParser;
+
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import edu.wpi.first.units.measure.Distance;
 
 public class LEDSubsystem extends SubsystemBase {
+
   private static final int kPort = 9;
-  private static final int kLength = 120;
-public static Object LEDPattern;
+  private static final int kLength = 60;
 
   private final AddressableLED m_led;
   private final AddressableLEDBuffer m_buffer;
 
+  
+  //final LEDPattern m_rainbow = LEDPattern.rainbow(255, 128);
+  final LEDPattern m_red = LEDPattern.solid(Color.kRed);
+
+  final Distance kLedSpacing = Meters.of(1 / 120);
+
+  //final LEDPattern m_scrollingRainbow =
+  //  m_rainbow.scrollAtAbsoluteSpeed(MetersPerSecond.of(1), kLedSpacing);
+
   public LEDSubsystem() {
     m_led = new AddressableLED(kPort);
-    m_buffer = new AddressableLEDBuffer(kLength);
-    m_led.setLength(kLength);
-    m_led.start();
 
+    m_buffer = new AddressableLEDBuffer(kLength);
+    m_led.setLength(m_buffer.getLength());
+
+    m_led.setData(m_buffer);
+    m_led.start();
     // Set the default command to turn the strip off, otherwise the last colors written by
     // the last command to run will continue to be displayed.
     // Note: Other default patterns could be used instead!
-    setDefaultCommand(runPattern(LEDPattern.solid(Color.kBlack)).withName("Off"));
+    // setDefaultCommand(runPattern(LEDPattern.solid(Color.kBlack)).withName("Off"));
   }
 
   @Override
   public void periodic() {
-    // Periodically send the latest LED color data to the LED strip for it to display
+    // Update the buffer with the rainbow animation
+    //m_scrollingRainbow.applyTo(m_buffer);
+    m_red.applyTo(m_buffer);
+    // Set the LEDs
     m_led.setData(m_buffer);
   }
 
