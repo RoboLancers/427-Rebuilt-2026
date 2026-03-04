7f2860bfd12bbbaae3cbefcca2fa507de70ee2b5
Connor Glazer <deerliver.contact@gmail.com>
2026-03-04 17:14:24 -0500

    Added Epilouge Logging
    Co-authored-by: Miles G <retropro29@users.noreply.github.com>

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,5 +1,6 @@
 package frc.robot;
 
+import edu.wpi.first.epilogue.Epilogue;
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -26,6 +27,7 @@ public class Robot extends TimedRobot {
     // and put our
     // autonomous chooser on the dashboard.
     DataLogManager.start();
+    Epilogue.bind(this);
 
     m_robotContainer = new RobotContainer();
     DataLogManager.start();
