6b0d356d1082cfea3fc520e9c037daf07dd4a4a5
Connor Glazer <deerliver.contact@gmail.com>
2026-03-04 17:14:35 -0500

    Added Epilouge Logging
    Co-authored-by: Miles G <retropro29@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -165,8 +165,8 @@ public class SwerveSubsystem extends SubsystemBase {
    * @param translationX Translation in the X direction
    * @param translationY Translation in the Y direction
    * @param angularRotationX Rotation of the robot to t
-   * @return Drive command.
-   *     <p>IT DOES SOMETHING
+   * @return Drive command. <div class = "javaComment">
+   *     <p>IT DOES SOMETHING</div> <style> .java </style>
    */
   public Command driveCommand(
       DoubleSupplier translationX, DoubleSupplier translationY, DoubleSupplier angularRotationX) {
