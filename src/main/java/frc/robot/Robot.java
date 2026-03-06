bf346f26be5011e6b355c1bc8f35d1d2af73175b
Connor Glazer <deerliver.contact@gmail.com>
2026-03-06 10:52:03 -0500

    Added Epilouge logging (#27)
    # Description
    
    Added swerve logging via Epilouge 
    
    ## Type of change
    
    Please delete options that are not relevant.
    
    - [ X ] New feature (non-breaking change which adds functionality)

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,7 @@ import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.cscore.CvSink;
 import edu.wpi.first.cscore.CvSource;
 import edu.wpi.first.cscore.UsbCamera;
+import edu.wpi.first.epilogue.Epilogue;
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -73,6 +74,7 @@ public class Robot extends TimedRobot {
     // and put our
     // autonomous chooser on the dashboard.
     DataLogManager.start();
+    Epilogue.bind(this);
 
     m_robotContainer = new RobotContainer();
     DataLogManager.start();
