dec516c0ff530e1e911cd31b5f9b85f2bd3e7523
Carl <carl.macy.stanton@protonmail.com>
2026-03-06 10:49:09 -0500

    Fixed Spotless formatting.

--- src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -11,7 +11,6 @@ import com.pathplanner.lib.controllers.PPHolonomicDriveController;
 import com.pathplanner.lib.path.PathPlannerPath;
 import com.pathplanner.lib.util.swerve.SwerveSetpoint;
 import com.pathplanner.lib.util.swerve.SwerveSetpointGenerator;
-import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
