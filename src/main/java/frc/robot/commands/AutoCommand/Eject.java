5370a7643a6861800b774e79b7457a4fec2e3060
YiXi <10053830@philasd.org>
2026-03-11 17:31:50 -0400

    Created own custom command classes
    Co-authored-by: Nofail24 <Nofail24@users.noreply.github.com>
    Co-authored-by: Connor Glazer <deerliver.contact@gmail.com>

---
+++ src/main/java/frc/robot/commands/AutoCommand/Eject.java
@@ -0,0 +1,27 @@
+// Copyright (c) FIRST and other WPILIB contributors.
+// Open Source Softwarel youc an modify and/or share it under the terms of
+// the WPILIB BSD liscense file in the root directory of this project.
+
+package frc.robot.commands.AutoCommand;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class Eject extends Command {
+
+public Eject(){}
+
+
+ @Override
+public void initialize() {}
+
+@Override
+pubilc void execute() {}
+
+ @Override
+ public void end(boolean interrupted) {}
+ 
+@Override
+public boolean isFinished() {
+    return false;
+ }
+}
