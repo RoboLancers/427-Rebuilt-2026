b88a43f88e914460c7f59b4636eaecc29a26af50
Sarah <lunyuhua@gmail.com>
2026-03-29 12:28:33 -0400

    removed intake.updateTelemetry();

--- src/main/java/frc/robot/subsystems/IntakeShooter/IntakeShooter.java
+++ src/main/java/frc/robot/subsystems/IntakeShooter/IntakeShooter.java
@@ -103,7 +103,7 @@ public class IntakeShooter extends SubsystemBase {
   public void periodic() {
     // This method will be called once per scheduler run
     // SmartDashboard.putNumber("Fuel Number", FuelCounter);
-    intake.updateTelemetry();
+    // intake.updateTelemetry();
     // boolean GamePiece = isGamePieceIn();
     // if (GamePiece == true) {
     //   FuelCounter += 1;
