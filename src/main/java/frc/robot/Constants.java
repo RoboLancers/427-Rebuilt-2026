a448fafa159e89edfe5e1cb2a30555f9c86ce528
Connor Glazer <deerliver.contact@gmail.com>
2026-06-30 12:35:25 -0400

    Changed Intake Speed to make intake work and not shoot :))

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -229,7 +229,7 @@ public final class Constants {
     public static final int FuelLimit = 8;
 
     public static final double IntakingFeeder = 3000;
-    public static final double IntakingIntake = 3500;
+    public static final double IntakingIntake = 1000;
 
     public static final double EjectingFeeder = -3500;
     public static final double EjectingIntake = -3500;
